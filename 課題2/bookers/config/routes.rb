20cb63ae4c0b9603ad77df0ace9b0609958c4090
EC2 Default User <ec2-user@ip-172-31-89-77.ec2.internal>
2022-04-01 16:59:12 +0000

    [Add] top, index, create, show

--- 課題2/bookers/config/routes.rb
+++ 課題2/bookers/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/edit'
+  get 'books/:id' => 'books#show', as: 'book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
