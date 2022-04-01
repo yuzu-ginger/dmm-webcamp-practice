e0e69bdc7a58dc6cc9e0e73213e8d99e70236109
EC2 Default User <ec2-user@ip-172-31-89-77.ec2.internal>
2022-04-01 19:12:08 +0000

    [Update] second

--- 課題2/bookers/app/controllers/books_controller.rb
+++ 課題2/bookers/app/controllers/books_controller.rb
@@ -1,15 +1,16 @@
 class BooksController < ApplicationController
-  def new
-  end
-  
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def index
-    @book = Book.all
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -17,6 +18,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   
   private
--- 課題2/bookers/config/routes.rb
+++ 課題2/bookers/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
-  get 'books/edit'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get 'books/:id' => 'books#show', as: 'book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
