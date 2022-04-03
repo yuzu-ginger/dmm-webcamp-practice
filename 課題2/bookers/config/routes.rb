8af4b368717b7db2cc3d1debe612acb1c3818ca4
EC2 Default User <ec2-user@ip-172-31-89-77.ec2.internal>
2022-04-03 05:34:22 +0000

    [Finish] アプリケーション基礎編

--- 課題2/bookers/config/routes.rb
+++ 課題2/bookers/config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   root to: 'books#top'
   resources :books
-  #post 'books' => 'books#create'
-  #get 'books' => 'books#index', as: 'books_path'
-  #get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  #get 'books/:id' => 'books#show', as: 'book'
-  #patch 'books/:id' => 'books#update', as: 'update_book'
-  #delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
